e2afa114bc61a4d27c7f126b06cd035d1bbf6565
bygu4 <sacha05bugaev@gmail.com>
2025-10-29 22:06:46 +0300

    add solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use std::fs;
+use std::io;
 
+fn main() {
+    let mut buf = String::new();
+
+    match io::stdin().read_line(&mut buf) {
+        Ok(_) => match fs::read(buf.trim()) {
+            Ok(_) => println!("success"),
+            _ => println!("failure"),
+        },
+        _ => println!("failure"),
+    }
 }
